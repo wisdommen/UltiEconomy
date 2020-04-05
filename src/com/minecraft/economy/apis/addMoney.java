91e8154a41536cb5da7f2b7473183c3f2d507dd2
wisdommen <xia.erich03@gmail.com>
2020-04-05 10:51:17 +1000

    修复无法从后台给玩家转账的bug 修复了玩家金币数为0时无法增加金币的bug

--- src/com/minecraft/economy/apis/addMoney.java
+++ src/com/minecraft/economy/apis/addMoney.java
@@ -14,7 +14,7 @@ public class addMoney {
         try {
             assert amount >= 0;
             if (checkMoney.player_file_exists(player_name)) {
-                if (checkMoney.check(player_name) > 0) {
+                if (checkMoney.check(player_name) >= 0) {
                     YamlConfiguration config = checkMoney.load_config(checkMoney.get_player_file(player_name));
                     config.set("money", checkMoney.check(player_name) + amount);
                     config.save(checkMoney.get_player_file(player_name));
